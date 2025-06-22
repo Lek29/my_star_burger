ff99344000ee024d3695ffc6bceac0d0c5b0a1b2
Lek <ligioner29@mail.ru>
2025-06-23 06:28:31 +0800

    used ModelSerializer in foodcartapp/serializers

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -10,24 +10,28 @@ class OrderItemSerializer(serializers.Serializer):
     quantity = serializers.IntegerField(min_value=1)
 
 
-class OrderSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+class OrderSerializer(serializers.ModelSerializer):
     firstname = serializers.CharField(max_length=100, source='client_name')
     lastname = serializers.CharField(max_length=100, source='surname')
     phonenumber = PhoneNumberField(region='RU', source='phone')
     address = serializers.CharField(max_length=255, source='delivery_address')
-
     products = OrderItemSerializer(many=True, allow_empty=False, write_only=True)
 
+    class Meta:
+        model = Order
+        fields = [
+            'id',
+            'firstname',
+            'lastname',
+            'phonenumber',
+            'address',
+            'products',
+        ]
     def create(self, validated_data):
         order_items_payload = validated_data.pop('products')
+
         with transaction.atomic():
-            order_instance = Order.objects.create(
-                client_name=validated_data['client_name'],
-                surname=validated_data['surname'],
-                phone=validated_data['phone'],
-                delivery_address=validated_data['delivery_address']
-            )
+            order_instance = super().create(validated_data)
 
             for item_payload in order_items_payload:
                 product_id = item_payload['product']
